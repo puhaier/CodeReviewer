dce6297ef86d1d4108e7c3d5e14ff19bc94bc80c
puhaier <haier.pu@qq.com>
2018-07-30 21:34:40 +0800

    test

---
+++ src/com/codereviewer/util/DownloadFiles.java
@@ -0,0 +1,13 @@
+package com.codereviewer.util;
+
+import javax.swing.*;
+
+public class DownloadFiles {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton startDownloadButton;
+
+}
